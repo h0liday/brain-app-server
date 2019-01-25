2433dc3ea50dcb0688a0bf26c40c92d9af298765
h0l1day <yarechek.arabichek@gmail.com>
2019-01-25 17:04:46 +0100

    hello1

--- server.js
+++ server.js
@@ -24,36 +24,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bananas',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 })
